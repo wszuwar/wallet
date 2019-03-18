f375d0ec33764c4ad8659a3cd977782de9318d18
“wszuwar <msuwr315@gmail.com>
2019-03-18 04:55:14 +0100

    third commit date

--- src/main/java/com/crud/wallet/model/Wallet.java
+++ src/main/java/com/crud/wallet/model/Wallet.java
@@ -1,17 +1,13 @@
 package com.crud.wallet.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.validator.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.Size;
 import java.util.Date;
 
 
@@ -27,8 +23,9 @@ public class Wallet {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-
-   @DateTimeFormat(pattern = "YYYY-MM-dd")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    @DateTimeFormat(pattern = "YYYY-MM-dd")
     @Column(name = "date")
     private Date date;
 
@@ -44,12 +41,5 @@ public class Wallet {
     @Column(name = "money_add")
     private Long moneyAdd;
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date createdAt) {
-        this.date = createdAt;
-    }
 
 }
